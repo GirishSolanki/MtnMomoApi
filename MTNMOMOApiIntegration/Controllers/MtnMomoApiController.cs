c513c071c924787f96b1e21121ddaa1eeb1dd870
Girish Solanki <solanki_girish@rocketmail.com>
2022-03-07 23:51:41 +0200

    pushed changes to git hub

---
+++ MTNMOMOApiIntegration/Controllers/MtnMomoApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MTNMOMOApiIntegration.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MtnMomoApiController : ControllerBase
+    {
+    }
+}
---
+++ MTNMOMOApiIntegration/Database/MtnMomoApiDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MTNMOMOApiIntegration.Database
+{
+    public class MtnMomoApiDataContext : DbContext
+    {
+        public MtnMomoApiDataContext(DbContextOptions<MtnMomoApiDataContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
